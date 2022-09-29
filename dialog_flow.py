61222dd9906f0a6b1fc8da6b7e72e7731d586426
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-09-29 13:00:58 +0300

    Automatically intent loading

--- dialog_flow.py
+++ dialog_flow.py
@@ -1,3 +1,8 @@
+import argparse
+import pathlib
+import json
+
+from environs import Env
 from google.cloud import dialogflow
 
 
@@ -26,3 +31,47 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         )
         print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 '''
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Используйте для загрузки фраз')
+    parser.add_argument('--json_path', type=pathlib.Path, help='Путь к JSON файлу')
+    args = parser.parse_args()
+    if args.json_path:
+        env = Env()
+        env.read_env()
+        project_id = env("PROJECT_ID")
+        with open(args.json_path, 'rb') as file:
+            intents_payload = json.load(file)
+        for display_name, payload in intents_payload.items():
+            create_intent(project_id, display_name, payload['questions'], [payload['answer']])
